35c9be068379c170121cc089fef4c6c38cfca079
jandersen747 <jonandersenofficial@hotmail.com>
2021-03-17 23:47:23 +0100

    w style

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
+app.use('/public', express.static('public'));
 app.use(expressEjsLayouts);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))

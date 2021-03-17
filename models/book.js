a501e1819afa476387eb321f92ff2aa730c7e2e6
jandersen747 <jonandersenofficial@hotmail.com>
2021-03-17 21:54:37 +0100

    2

--- models/book.js
+++ models/book.js
@@ -23,7 +23,7 @@ const bookSchema = new mongoose.Schema({
     author: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: 'Author'
+        ref: 'Author',
     }
 })
 
